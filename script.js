abc271cc389c1d63845e6d2fffb4111651ce4953
unknown <lassana.traore2020@campus-eni.fr>
2021-08-07 13:09:33 +0000

    add nav responsiveness

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("test");
+
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "sidebar") {
+      x.className += " responsive";
+    } else {
+      x.className = "sidebar";
+    }
+  }
